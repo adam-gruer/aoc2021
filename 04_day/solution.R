4ef4371e0c5cb7b2b8ef8611eefe0728822ec272
Adam Gruer <adam.gruer@epa.vic.gov.au>
2022-08-11 21:48:19 +1000

    04_day part 1 complete

--- 04_day/solution.R
+++ 04_day/solution.R
@@ -1,8 +1,51 @@
 #part 1
 #test
 call <- scan("04_day/data/test_data", sep = ",", nlines = 1)
-boards <- scan("04_day/data/test_data", skip = 1)
+boards_vector <- scan("04_day/data/test_data", skip = 1)
+board_size <- c(5,5)
+n_boards <- length(boards_vector) /prod(board_size)
+boards <- array(boards_vector, dim = c(board_size,n_boards))
+#boards <- aperm(boards, c(2,1,3))
+marked_boards_init <- array(FALSE, dim = dim(boards) )
 
-boards <- array(boards, dim = c(5,5,3))
+#boards_bool <- boards == call[1]
+#boards_bool <- boards_bool + (boards == call[2])
+#apply(boards_bool, c(1,3), all)
 
-boards_bool <- boards == call[1]
+
+mark_boards <- function(marked_boards, number){
+  marked_boards + (boards == number)
+}
+
+check_bingo <- function(marked_boards){
+  
+  bingo_board <- c(check_rows(marked_boards),
+  check_columns(marked_boards))[2]
+
+  sum(boards[,,bingo_board][!as.logical(marked_boards[,,bingo_board])])
+  
+  
+}
+
+check_rows <- function(marked_boards){
+  apply(marked_boards,c(1,3), all) |>
+    which(arr.ind = TRUE)
+}
+
+check_columns <- function(marked_boards){
+  apply(marked_boards,c(2,3), all) |>
+    which(arr.ind = TRUE)
+}
+
+check_bingo(marked_boards)
+
+bingo <- NA_integer_
+marked_boards <- marked_boards_init
+i <- 0
+while(is.na(bingo)){
+  i <- i + 1
+  marked_boards <- mark_boards(marked_boards, call[i] )
+  bingo <- check_bingo(marked_boards)
+}
+
+call[i] * bingo 
